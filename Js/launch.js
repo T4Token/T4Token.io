c7ff6de70243f8d38c4529f121fc002c0fba647f
T4Token <t4token@gmail.com>
2022-12-19 18:18:54 +0100

    New Change

--- Js/launch.js
+++ Js/launch.js
@@ -1,127 +1,122 @@
-(() => {
-  // Specify the deadline date
-  const deadlineDate = new Date('December 19, 2022 23:59:59').getTime();
+function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, 
+    { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}class Countdown extends React.Component {constructor(...args) {super(...args);_defineProperty(this, "state",
+    {
+      days: undefined,
+      hours: undefined,
+      minutes: undefined,
+      seconds: undefined });}
 
-  // Cache all countdown boxes into consts
-  const countdownDays = document.querySelector('.countdown__days .number');
-  const countdownHours = document.querySelector('.countdown__hours .number');
-  const countdownMinutes = document.querySelector('.countdown__minutes .number');
-  const countdownSeconds = document.querySelector('.countdown__seconds .number');
 
-  // Update the count down every 1 second (1000 milliseconds)
-  setInterval(() => {
-    // Get current date and time
-    const currentDate = new Date().getTime();
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      const { timeTillDate, timeFormat } = this.props;
+      const then = moment(timeTillDate, timeFormat);
+      const now = moment();
+      const countdown = moment(then - now);
+      const days = countdown.format('D');
+      const hours = countdown.format('HH');
+      const minutes = countdown.format('mm');
+      const seconds = countdown.format('ss');
 
-    // Calculate the distance between current date and time and the deadline date and time
-    const distance = deadlineDate - currentDate;
-
-    // Calculations the data for remaining days, hours, minutes and seconds
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
-    const minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
-    const seconds = Math.floor(distance % (1000 * 60) / 1000);
-
-    // Insert the result data into individual countdown boxes
-    countdownDays.innerHTML = days;
-    countdownHours.innerHTML = hours;
-    countdownMinutes.innerHTML = minutes;
-    countdownSeconds.innerHTML = seconds;
-  }, 1000);
-})();
-
-// Necessary GSAP libs:
-// * TweenLite.min.js
-// * TimelineLite.min.js
-// * AttrPlugin.min.js
-// * CSSPlugin.min.js
-//
-// Total size is about 80kB
-
-window.onload = function () {
-  // Keep graphics fit the screen for small devices
-  mobileScale();
-
-  // Loop timeline
-  let tl = new TimelineLite({
-      onComplete: function() {
-          this.restart();
-      }
-  });
-
-  // Open the laptop
-  tl.to('.laptop-lid', 0.1, {
-      fill: '#313638',
-      stroke: 'none',
-      ease: Linear.easeNone
-  }, '+=1');
-  tl.to('.laptop-lid', 0.6, {
-      attr: {d: 'M 70,240 l 341.643 0 l 0 -215.73 l -341.643 0 l 0 215.73 z M 4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbRyFTLRNyDmT1a1boZV048 18.048 l 0 231.663 z'},
-      ease: Power1.easeOut
-  }, '-=0.1');
-
-  // Switch on the screen
-  tl.to(['.screen-bg', '.grid', '.logo'], 0.1, {
-      opacity: 1,
-      ease: Power1.easeOut
-  });
-
-  // Draw charts
-  tl.to('.chart', 0.5, {
-      strokeDashoffset: 0,
-      ease: Power1.easeIn
-  });
-
-  // make boxes fly from the laptop center + fade in the boxs
-  const delta = [
-      {x: 12, y: 17},
-      {x: -15, y: 13},
-      {x: -14, y: -13},
-      {x: 21, y: 0},
-      {x: -3, y: 23},
-      {x: -21, y: 11},
-      {x: -21, y: 2},
-      {x: -20, y: 5},
-      {x: 11, y: -27},
-      {x: 24, y: -11},
-      {x: 21, y: 9}
-  ];
-  const textBoxes = Array.from(document.getElementsByClassName('text-block'));
-  for (idx in delta) {
-      tl.fromTo(textBoxes[idx], 0.6, {
-          x: delta[idx].x,
-          y: delta[idx].y
-      }, {
-          x: 0,
-          y: 0,
-          opacity: 1,
-          ease: Power2.easeOut
-      }, 2.5);
+      this.setState({ days, hours, minutes, seconds });
+    }, 1000);
   }
 
-  // fade out the whole container
-  tl.to('.container', 0.5, {
-      opacity: 0
-  }, '+=1');
-};
-
-window.onresize = mobileScale;
-
-// Keep graphics fit the screen for small devices
-function mobileScale() {
-  scale = Math.min(
-      // the container is (577 x 418) px initially
-      window.innerWidth / 577,
-      window.innerHeight / 418
-  );
-  if (scale < 1) {
-      TweenLite.set('.container', {
-          scale: scale,
-          transformOrigin: 'center center'
-      })
+  componentWillUnmount() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
   }
+
+  render() {
+    const { days, hours, minutes, seconds } = this.state;
+    const daysRadius = mapNumber(days, 30, 0, 0, 360);
+    const hoursRadius = mapNumber(hours, 24, 0, 0, 360);
+    const minutesRadius = mapNumber(minutes, 60, 0, 0, 360);
+    const secondsRadius = mapNumber(seconds, 60, 0, 0, 360);
+
+    if (!seconds) {
+      return null;
+    }
+
+    return /*#__PURE__*/(
+      React.createElement("div", null, /*#__PURE__*/
+      React.createElement("h1", null, "Countdown"), /*#__PURE__*/
+      React.createElement("div", { className: "countdown-wrapper" },
+      days && /*#__PURE__*/
+      React.createElement("div", { className: "countdown-item" }, /*#__PURE__*/
+      React.createElement(SVGCircle, { radius: daysRadius }),
+      days, /*#__PURE__*/
+      React.createElement("span", null, "days")),
+
+
+      hours && /*#__PURE__*/
+      React.createElement("div", { className: "countdown-item" }, /*#__PURE__*/
+      React.createElement(SVGCircle, { radius: hoursRadius }),
+      hours, /*#__PURE__*/
+      React.createElement("span", null, "hours")),
+
+
+      minutes && /*#__PURE__*/
+      React.createElement("div", { className: "countdown-item" }, /*#__PURE__*/
+      React.createElement(SVGCircle, { radius: minutesRadius }),
+      minutes, /*#__PURE__*/
+      React.createElement("span", null, "minutes")),
+
+
+      seconds && /*#__PURE__*/
+      React.createElement("div", { className: "countdown-item" }, /*#__PURE__*/
+      React.createElement(SVGCircle, { radius: secondsRadius }),
+      seconds, /*#__PURE__*/
+      React.createElement("span", null, "seconds")))));
+
+
+
+
+
+  }}
+
+
+const SVGCircle = ({ radius }) => /*#__PURE__*/
+React.createElement("svg", { className: "countdown-svg" }, /*#__PURE__*/
+React.createElement("path", { fill: "none", stroke: "#333", "stroke-width": "4", d: describeArc(50, 50, 48, 0, radius) }));
+
+
+
+ReactDOM.render( /*#__PURE__*/
+React.createElement(Countdown, {
+  timeTillDate: "12 28 2022, 10:00 am",
+  timeFormat: "MM DD YYYY, h:mm a" }),
+
+document.getElementById('app'));
+
+
+// From stackoverflow: https://stackoverflow.com/questions/5736398/how-to-calculate-the-svg-path-for-an-arc-of-a-circle
+function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+  var angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
+
+  return {
+    x: centerX + radius * Math.cos(angleInRadians),
+    y: centerY + radius * Math.sin(angleInRadians) };
+
 }
 
-$(window).on("load", function() {
-  $(".loader-wrapper").fadeout("slow");
-})
+function describeArc(x, y, radius, startAngle, endAngle) {
+
+  var start = polarToCartesian(x, y, radius, endAngle);
+  var end = polarToCartesian(x, y, radius, startAngle);
+
+  var largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+
+  var d = [
+  "M", start.x, start.y,
+  "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y].
+  join(" ");
+
+  return d;
+}
+
+// Stackoverflow: https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+function mapNumber(number, in_min, in_max, out_min, out_max) {
+  return (number - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
